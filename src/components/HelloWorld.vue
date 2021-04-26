e9f914d814fdacf101fb651456de267ac3b87aec
Alex Kokkoros <alexkokkoros@Alexs-MacBook-Pro-2.local>
2021-04-25 22:13:06 -0400

    added search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,61 @@
-<template>
-<body>
+<template><body>
+  <form name="search" v-if="home">
+    <label> Search Properties</label><br />
+    <div class="group">
+      <label class="form-label" for="Address">Address</label> <br />
+      <input type="text" id = "Address" v-model="searchAddress" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="Apt">Apt</label> <br />
+      <input class="form-field" name="Apt" id="Apt" v-model="searchApt" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="yr_built">Year Built</label> <br />
+      <input class="form-field" name="yr_built" id="yr_built" v-model="searchYearBuilt" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="long">Longitude</label> <br />
+      <input class="form-field" name="long" id="long" v-model="searchLong" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="lat">Latitude</label> <br />
+      <input class="form-field" name="lat" id="lat" v-model="searchLat" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="nbhd">Neighborhood</label> <br />
+      <input class="form-field" name="nbhd" id="nbhd" v-model="searchNbhd" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="block">Block</label> <br />
+      <input class="form-field" name="block" id="block" v-model="searchBlock" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="lot">Lot</label> <br />
+      <input class="form-field" name="lot" id="lot" v-model="searchLot" lazy>  
+    </div>
+    <div class="group">
+      <label class="form-label" for="bldgcls">Building Class</label> <br />
+    <input class="form-field" name="bldgcls" id="bldgcls" v-model="searchBldgCls" lazy>
+    </div>
+    <div class="group">
+      <label class="form-label" for="taxcls">Tax Class</label> <br />
+    <input class="form-field" name="taxcls" id="taxcls" v-model="searchTaxCls" lazy>
+    </div>
+    <button type="submit" @click="searchProperties()"> Search </button>
+  </form>
   <table border=1 v-if="home">
+    <caption>Properties</caption>
     <tr>
-    <th>Address</th>
-    <th>Apt</th>
-    <th>Year Built</th>
-    <th>Longitude</th>
-    <th>Latitude</th>
-    <th>Neighborhood</th>
-    <th>Block</th>
-    <th>Lot</th>
-    <th>Building Class</th>
-    <th>Tax Class</th>
+      <th>Address</th>
+      <th>Apt</th>
+      <th>Year Built</th>
+      <th>Longitude</th>
+      <th>Latitude</th>
+      <th>Neighborhood</th>
+      <th>Block</th>
+      <th>Lot</th>
+      <th>Building Class</th>
+      <th>Tax Class</th>
     </tr>
     <tr v-for="property in properties" v-bind:key="property" v-on:click="findsales(property)">
         <td>{{ property.address }}</td>
@@ -31,29 +75,113 @@
         <td v-on:click="getProperties(nxt)"><p v-if="nxt">Next--&#62;</p></td>
     </tr>
   </table>
+
+
+  <table border=1 v-if="filteredpropertiesview">
+    <caption>Properties</caption>
+    <tr>
+      <th colspan="10">Results</th>
+    </tr>
+    <tr>
+      <th colspan="10">Searching For</th>
+    </tr>
+    <tr>
+      <th>Address</th>
+      <th>Apt</th>
+      <th>Year Built</th>
+      <th>Longitude</th>
+      <th>Latitude</th>
+      <th>Neighborhood</th>
+      <th>Block</th>
+      <th>Lot</th>
+      <th>Building Class</th>
+      <th>Tax Class</th>
+    </tr>
+    <tr>
+      <th>{{searchAddress}}</th>
+      <th>{{searchApt}}</th>
+      <th>{{searchYearBuilt}}</th>
+      <th>{{searchLong}}</th>
+      <th>{{searchLat}}</th>
+      <th>{{searchNbhd}}</th>
+      <th>{{searchBlock}}</th>
+      <th>{{searchLot}}</th>
+      <th>{{searchBldgCls}}</th>
+      <th>{{searchTaxCls}}</th>
+    </tr>
+    <tr>
+      <th>Address</th>
+      <th>Apt</th>
+      <th>Year Built</th>
+      <th>Longitude</th>
+      <th>Latitude</th>
+      <th>Neighborhood</th>
+      <th>Block</th>
+      <th>Lot</th>
+      <th>Building Class</th>
+      <th>Tax Class</th>
+    </tr>
+    <tr v-for="property in fproperties" v-bind:key="property" v-on:click="findsales(item)">
+        <td>{{ property.address }}</td>
+        <td>{{ property.apt }}</td>
+        <td>{{ property.yr_built }}</td>
+        <td>{{ property.longitude }}</td>
+        <td>{{ property.lat }}</td>
+        <td>{{ property.nbhd }}</td>
+        <td>{{ property.pblock }}</td>
+        <td>{{ property.lot }}</td>
+        <td>{{ property.bldg_cls_p }}</td>
+        <td>{{ property.tax_cls_p }}</td>
+    </tr>
+  </table>
+
+
   <table border=1 v-if="propertyview">
     <tr>
-        <td>bbl_id</td>
-        <td>address</td>
-        <td>apt</td>
-        <td>zip</td>
-        <td>syear</td>
-        <td>easmnt</td>
-        <td>tax_cls_s</td>
-        <td>blog_cls_p</td>
-        <td>sale_date</td>
-        <td>price</td>
-        <td>res_unit</td>
-        <td>com_unit</td>
-        <td>tot_unit</td>
-        <td>bldg_ctgy</td>
-        <td>message</td>
+        <th>Building ID</th>
+        <th>Address</th>
+        <th>Apt</th>
+        <th>Year Built</th>
+        <th>Longitude</th>
+        <th>Latitude</th>
+        <th>Neighborhood</th>
+        <th>Block</th>
+        <th>Lot</th>
+        <th>Building Class</th>
+        <th>Tax Class</th>
+    </tr>
+    <tr>
+      <td> {{propertybbl}} </td>
+      <td> {{propertyadd}} </td>
+      <td> {{propertyapt}} </td>
+      <td> {{propertyyrbuilt}} </td>
+      <td> {{propertylong}} </td>
+      <td> {{propertylat}} </td>
+      <td> {{propertyngbd}} </td>
+      <td> {{propertyblock}} </td>
+      <td> {{propertylot}} </td>
+      <td> {{propertybldgcls}} </td>
+      <td> {{propertytaxcls}} </td>
+    </tr>
+    <tr>
+      <td v-on:click="propertyview=false; home=true"><p>&#60;--Back</p></td>    
+    </tr>
+  </table>
+  <table v-if="propertyview">
+    <tr>
+      <td>Sale Year</td>
+      <td>Sale Easement</td>
+      <td>Tax Class at Sale</td>
+      <td>Building Class at Sale</td>
+      <td>Sale Date</td>
+      <td>Sale Price</td>
+      <td>Resedential Units at Time of Sale</td>
+      <td>Commerical Units at Time of Sale</td>
+      <td>Total Units at Time of Sale</td>
+      <td>Building Category at Time of Sale</td>
+      <td>Notes on Sale</td>
     </tr>
     <tr v-for="sale in sales" v-bind:key="sale">
-        <td>{{ sale.bbl_id }}</td>
-        <td>{{ sale.address }}</td>
-        <td>{{ sale.apt }}</td>
-        <td>{{ sale.zip }}</td>
         <td>{{ sale.syear }}</td>
         <td>{{ sale.easmnt }}</td>
         <td>{{ sale.tax_cls_s }}</td>
@@ -65,28 +193,6 @@
         <td>{{ sale.tot_unit }}</td>
         <td>{{ sale.bldg_ctgy }}</td>
         <td>{{ sale.message }}</td>
-    </tr>
-    <tr>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-        <td><input/></td>
-    </tr>
-    <tr>
-        <td v-on:click="propertyview=false; home=true"><p>&#60;--Back</p></td>
-        <td colspan="9">Showing Sales</td>
-        
     </tr>
   </table>
   </body>
@@ -107,14 +213,43 @@ export default {
   data: function () {
     return {
       properties: [],
+      fproperties: [],
       sales:[],
       count:0,
+      filteredcount:0,
       nxt:"",
+      fnxt:"",
       prev:"",
+      fprev:"",
       start:0,
+      fstart:0,
       end:0,
+      fend:0,
       home:true,
-      propertyview:false
+      propertyview:false,
+      filteredpropertiesview:false,
+      property: "",
+      propertybbl: "",
+      propertyadd: "",
+      propertyapt:"",
+      propertyyrbuilt:"",
+      propertylong:"",
+      propertylat:"",
+      propertyngbd:"",
+      propertyblock:"",
+      propertylot:"",
+      propertybldgcls:"",
+      propertytaxcls: "",
+      searchAddress: null,
+      searchApt: null,
+      searchYearBuilt: null,
+      searchLong: null,
+      searchLat: null,
+      searchNbhd: null,
+      searchBlock: null,
+      searchLot: null,
+      searchBldgCls: null,
+      searchTaxCls: null
     }
   },
   methods: {
@@ -139,6 +274,7 @@ export default {
             })
             })
         },
+    
     getProperties: function (page) {
         let url='properties'
         if (page) {
@@ -155,21 +291,55 @@ export default {
         })
         console.log(this.nxt)
     },
+    searchProperties: function(){
+      this.home = false
+      this.filteredpropertiesview = true
+      let url='filterProperties'
+      
+      axios.get(encodeURI(url),{params:{
+        address:this.searchAddress,
+        apt:this.searchApt,
+        yr_built:this.searchYearBuilt,
+        longitude:this.searchLong,
+        lat:this.searchLat,
+        nbhd:this.searchNbhd,
+        pblock:this.searchBlock,
+        lot:this.searchLot,
+        bldg_cls_p:this.searchBldgCls,
+        tax_cls_p:this.searchTaxCls}}).then(resp =>{
+        console.log(resp.data)
+        this.fproperties = resp.data
+      })
+    },
+
     findsales: function (property) {
         this.home=false
+        this.filteredpropertiesview = false
         this.propertyview=true
-        let url='property/?bbl_id='+property.bbl_id+'&address='+property.address+'&apt='+property.apt
-        this.get(url).then(response => {
+        this.property = property
+        this.propertybbl = property.bbl_id
+        this.propertyadd = property.address
+        this.propertyapt = property.apt
+        this.propertyyrbuilt = property.yr_built
+        this.propertylong = property.longitude
+        this.propertylat = property.lat
+        this.propertyngbd = property.nbhd
+        this.propertyblock = property.pblock
+        this.propertylot = property.lot
+        this.propertybldgcls = property.bldg_cls_p
+        this.propertytaxcls = property.tax_cls_p
+
+        
+
+        let url='salesforproperty/?bbl_id='+property.bbl_id+'&address='+property.address+'&apt='+property.apt
+        this.get(encodeURI(url)).then(response => {
             console.log(response)
             this.sales= response.data.results
+            
         })
         console.log(this.sales)
     },
-    findsales2: function (property) {
-        console.log(property)
-        this.home=false
-        this.propertyview=true
-    }
+
   }
 }
 
@@ -177,22 +347,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-table {
-margin-left: auto;
-margin-right: auto;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.group{
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+form{
+  margin-top: 5%;
+  vertical-align: middle;
+  margin-left: auto;
+  margin-right:auto;
+  text-align:center;
+}
+table{
+  margin-top: 5%;
+  vertical-align: middle;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: white;
+  text-align: center;
 }
 </style>
